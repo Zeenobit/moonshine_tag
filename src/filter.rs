1fc7bdd246c5fa0be16f9847921e32c065c18eae
Zeenobit <zeenobit.exe@gmail.com>
2025-03-13 22:21:50 -0400

    Create filter.rs and add `TagFilter` constructors

---
+++ src/filter.rs
@@ -0,0 +1,245 @@
+use std::{
+    any::Any,
+    ops::{BitAnd, BitOr, Not},
+};
+
+use bevy_reflect::{
+    prelude::*, utility::NonGenericTypeInfoCell, ApplyError, GetTypeRegistration, OpaqueInfo,
+    ReflectMut, ReflectOwned, ReflectRef, TypeInfo, TypePath, TypeRegistration, Typed,
+};
+use serde::{Deserialize, Serialize};
+
+use crate::{IntoTags, Tags};
+
+/// A filter which may be used to test a set of tags for a match.
+///
+/// # Usage
+///
+/// Filters are serializable and may be combined with each other to create complex tag matching rules.
+///
+/// # Examples
+/// ```rust
+/// use moonshine_tag::{prelude::*, all, matches};
+///
+/// tags! { A, B, C };
+///
+/// let a: Tags = A.into_tags();
+/// let b: Tags = B.into_tags();
+/// let c: Tags = C.into_tags();
+/// let ab: Tags = [A, B].into_tags();
+///
+/// let a_or_b: TagFilter = A | B;
+/// assert!(a_or_b.allows(&a));
+/// assert!(a_or_b.allows(&b));
+/// assert!(!a_or_b.allows(&ab));
+/// assert!(!a_or_b.allows(&c));
+///
+/// let c_or_ab: TagFilter = C | [A, B];
+/// assert!(!c_or_ab.allows(&a));
+/// assert!(!c_or_ab.allows(&b));
+/// assert!(c_or_ab.allows(&ab));
+/// assert!(c_or_ab.allows(&c));
+/// ```
+#[derive(Clone, Serialize, Deserialize, Reflect)]
+pub enum TagFilter {
+    /// Matches no tags.
+    None,
+    /// Matches any set of tags.
+    Any,
+    /// Matches any set of tags which is exactly equal to the filter tags.
+    Eq(Tags),
+    /// Matches any set of tags which contains all of the filter tags.
+    All(Tags),
+    /// Matches any set of tags which contains any of the filter tags.
+    Some(Tags),
+    /// Matches any set of tags which matches both inner filters.
+    And(TagFilterDyn, TagFilterDyn),
+    /// Matches any set of tags which matches either inner filters.
+    Or(TagFilterDyn, TagFilterDyn),
+    /// Matches any set of tags which does not match the inner filter.
+    Not(TagFilterDyn),
+}
+
+impl TagFilter {
+    pub fn none() -> TagFilter {
+        TagFilter::Eq(().into_tags())
+    }
+
+    pub fn any() -> TagFilter {
+        TagFilter::Any
+    }
+
+    pub fn all(all: impl IntoTags) -> TagFilter {
+        TagFilter::All(all.into_tags())
+    }
+
+    pub fn some(some: impl IntoTags) -> TagFilter {
+        TagFilter::Some(some.into_tags())
+    }
+
+    pub fn not(not: impl Into<TagFilter>) -> TagFilter {
+        !not.into()
+    }
+
+    /// Returns `true` if this filter allows the given set of tags.
+    pub fn allows(&self, tags: &Tags) -> bool {
+        use TagFilter::*;
+        match self {
+            None => false,
+            Any => true,
+            Eq(a) => a == tags,
+            All(a) => a.is_subset(tags),
+            Some(a) => !a.is_disjoint(tags),
+            And(a, b) => a.allows(tags) && b.allows(tags),
+            Or(a, b) => a.allows(tags) || b.allows(tags),
+            Not(a) => !a.allows(tags),
+        }
+    }
+}
+
+impl Default for TagFilter {
+    fn default() -> Self {
+        Self::Any
+    }
+}
+
+impl<T: IntoTags> From<T> for TagFilter {
+    fn from(tags: T) -> Self {
+        Self::Eq(tags.into_tags())
+    }
+}
+
+impl<T: Into<TagFilter>> BitAnd<T> for TagFilter {
+    type Output = Self;
+
+    fn bitand(self, rhs: T) -> Self::Output {
+        Self::And(Box::new(self), Box::new(rhs.into()))
+    }
+}
+
+impl<T: Into<TagFilter>> BitOr<T> for TagFilter {
+    type Output = Self;
+
+    fn bitor(self, rhs: T) -> Self::Output {
+        Self::Or(Box::new(self), Box::new(rhs.into()))
+    }
+}
+
+impl Not for TagFilter {
+    type Output = TagFilter;
+
+    fn not(self) -> Self::Output {
+        Self::Not(Box::new(self))
+    }
+}
+
+type TagFilterDyn = Box<TagFilter>;
+
+impl PartialReflect for Box<TagFilter> {
+    fn get_represented_type_info(&self) -> Option<&'static TypeInfo> {
+        (**self).get_represented_type_info()
+    }
+
+    fn into_partial_reflect(self: Box<Self>) -> Box<dyn PartialReflect> {
+        (*self).into_partial_reflect()
+    }
+
+    fn as_partial_reflect(&self) -> &dyn PartialReflect {
+        (**self).as_partial_reflect()
+    }
+
+    fn as_partial_reflect_mut(&mut self) -> &mut dyn PartialReflect {
+        (**self).as_partial_reflect_mut()
+    }
+
+    fn try_into_reflect(self: Box<Self>) -> Result<Box<dyn Reflect>, Box<dyn PartialReflect>> {
+        (*self).try_into_reflect()
+    }
+
+    fn try_as_reflect(&self) -> Option<&dyn Reflect> {
+        (**self).try_as_reflect()
+    }
+
+    fn try_as_reflect_mut(&mut self) -> Option<&mut dyn Reflect> {
+        (**self).try_as_reflect_mut()
+    }
+
+    fn try_apply(&mut self, value: &dyn PartialReflect) -> Result<(), ApplyError> {
+        (**self).try_apply(value)
+    }
+
+    fn reflect_ref(&self) -> ReflectRef {
+        (**self).reflect_ref()
+    }
+
+    fn reflect_mut(&mut self) -> ReflectMut {
+        (**self).reflect_mut()
+    }
+
+    fn reflect_owned(self: Box<Self>) -> ReflectOwned {
+        (*self).reflect_owned()
+    }
+
+    fn clone_value(&self) -> Box<dyn PartialReflect> {
+        (**self).clone_value()
+    }
+}
+
+impl Reflect for Box<TagFilter> {
+    fn into_any(self: Box<Self>) -> Box<dyn Any> {
+        (*self).into_any()
+    }
+
+    fn as_any(&self) -> &dyn Any {
+        (**self).as_any()
+    }
+
+    fn as_any_mut(&mut self) -> &mut dyn Any {
+        (**self).as_any_mut()
+    }
+
+    fn into_reflect(self: Box<Self>) -> Box<dyn Reflect> {
+        (*self).into_reflect()
+    }
+
+    fn as_reflect(&self) -> &dyn Reflect {
+        (**self).as_reflect()
+    }
+
+    fn as_reflect_mut(&mut self) -> &mut dyn Reflect {
+        (**self).as_reflect_mut()
+    }
+
+    fn set(&mut self, value: Box<dyn Reflect>) -> Result<(), Box<dyn Reflect>> {
+        (**self).set(value)
+    }
+}
+
+impl FromReflect for Box<TagFilter> {
+    fn from_reflect(reflect: &dyn PartialReflect) -> Option<Self> {
+        TagFilter::from_reflect(reflect).map(Box::new)
+    }
+}
+
+impl TypePath for Box<TagFilter> {
+    fn type_path() -> &'static str {
+        TagFilter::type_path()
+    }
+
+    fn short_type_path() -> &'static str {
+        TagFilter::short_type_path()
+    }
+}
+
+impl Typed for Box<TagFilter> {
+    fn type_info() -> &'static TypeInfo {
+        static CELL: NonGenericTypeInfoCell = NonGenericTypeInfoCell::new();
+        CELL.get_or_set(|| TypeInfo::Opaque(OpaqueInfo::new::<Self>()))
+    }
+}
+
+impl GetTypeRegistration for Box<TagFilter> {
+    fn get_type_registration() -> TypeRegistration {
+        TypeRegistration::of::<Self>()
+    }
+}
--- src/lib.rs
+++ src/lib.rs
@@ -1,5 +1,12 @@
+pub mod prelude {
+    pub use crate::{
+        tags, GetEntityTags, GetTags, IntoTags, Tag, TagFilter, TagPlugin, Tags, WithTags,
+    };
+}
+
+mod filter;
+
 use std::{
-    any::Any,
     fmt,
     hash::{Hash, Hasher},
     ops::{BitAnd, BitOr, Not},
@@ -7,20 +14,12 @@ use std::{
 
 use bevy_app::{App, Plugin};
 use bevy_ecs::{component::ComponentId, prelude::*, world::DeferredWorld};
-use bevy_reflect::{
-    prelude::*, utility::NonGenericTypeInfoCell, ApplyError, GetTypeRegistration, OpaqueInfo,
-    ReflectMut, ReflectOwned, ReflectRef, TypeInfo, TypePath, TypeRegistration, Typed,
-};
+use bevy_reflect::prelude::*;
 use bevy_utils::HashSet;
 use once_cell::sync::Lazy;
 use serde::{Deserialize, Serialize};
 
-pub mod prelude {
-    pub use crate::{
-        tags, GetEntityTags, GetTags, IntoTags, Tag, TagFilter, TagFilterAnd, TagFilterOr,
-        TagPlugin, Tags, WithTags,
-    };
-}
+pub use filter::TagFilter;
 
 /// A [`Plugin`] required to register tag related types for reflection.
 pub struct TagPlugin;
@@ -425,259 +424,6 @@ impl IntoTags for Tags {
     }
 }
 
-/// A filter which may be used to test a set of tags for a match.
-///
-/// # Usage
-///
-/// Filters are serializable and may be combined with each other to create complex tag matching rules.
-///
-/// # Examples
-/// ```rust
-/// use moonshine_tag::{prelude::*, all, matches};
-///
-/// tags! { A, B, C };
-///
-/// let a: Tags = A.into_tags();
-/// let b: Tags = B.into_tags();
-/// let c: Tags = C.into_tags();
-/// let ab: Tags = [A, B].into_tags();
-///
-/// let a_or_b: TagFilter = A | B;
-/// assert!(a_or_b.allows(&a));
-/// assert!(a_or_b.allows(&b));
-/// assert!(!a_or_b.allows(&ab));
-/// assert!(!a_or_b.allows(&c));
-///
-/// let c_or_ab: TagFilter = C | [A, B];
-/// assert!(!c_or_ab.allows(&a));
-/// assert!(!c_or_ab.allows(&b));
-/// assert!(c_or_ab.allows(&ab));
-/// assert!(c_or_ab.allows(&c));
-/// ```
-#[derive(Clone, Serialize, Deserialize, Reflect)]
-pub enum TagFilter {
-    /// Matches no tags.
-    None,
-    /// Matches any set of tags.
-    Any,
-    /// Matches any set of tags which is exactly equal to the filter tags.
-    Eq(Tags),
-    /// Matches any set of tags which contains all of the filter tags.
-    All(Tags),
-    /// Matches any set of tags which contains any of the filter tags.
-    Some(Tags),
-    /// Matches any set of tags which matches both inner filters.
-    And(TagFilterDyn, TagFilterDyn),
-    /// Matches any set of tags which matches either inner filters.
-    Or(TagFilterDyn, TagFilterDyn),
-    /// Matches any set of tags which does not match the inner filter.
-    Not(TagFilterDyn),
-}
-
-impl TagFilter {
-    /// Returns `true` if this filter allows the given set of tags.
-    pub fn allows(&self, tags: &Tags) -> bool {
-        use TagFilter::*;
-        match self {
-            None => false,
-            Any => true,
-            Eq(a) => a == tags,
-            All(a) => a.is_subset(tags),
-            Some(a) => !a.is_disjoint(tags),
-            And(a, b) => a.allows(tags) && b.allows(tags),
-            Or(a, b) => a.allows(tags) || b.allows(tags),
-            Not(a) => !a.allows(tags),
-        }
-    }
-}
-
-impl Default for TagFilter {
-    fn default() -> Self {
-        Self::Any
-    }
-}
-
-impl<T: IntoTags> From<T> for TagFilter {
-    fn from(tags: T) -> Self {
-        Self::Eq(tags.into_tags())
-    }
-}
-
-impl<T: Into<TagFilter>> BitAnd<T> for TagFilter {
-    type Output = Self;
-
-    fn bitand(self, rhs: T) -> Self::Output {
-        Self::And(Box::new(self), Box::new(rhs.into()))
-    }
-}
-
-impl<T: Into<TagFilter>> BitOr<T> for TagFilter {
-    type Output = Self;
-
-    fn bitor(self, rhs: T) -> Self::Output {
-        Self::Or(Box::new(self), Box::new(rhs.into()))
-    }
-}
-
-impl Not for TagFilter {
-    type Output = TagFilter;
-
-    fn not(self) -> Self::Output {
-        Self::Not(Box::new(self))
-    }
-}
-
-type TagFilterDyn = Box<TagFilter>;
-
-impl PartialReflect for Box<TagFilter> {
-    fn get_represented_type_info(&self) -> Option<&'static TypeInfo> {
-        (**self).get_represented_type_info()
-    }
-
-    fn into_partial_reflect(self: Box<Self>) -> Box<dyn PartialReflect> {
-        (*self).into_partial_reflect()
-    }
-
-    fn as_partial_reflect(&self) -> &dyn PartialReflect {
-        (**self).as_partial_reflect()
-    }
-
-    fn as_partial_reflect_mut(&mut self) -> &mut dyn PartialReflect {
-        (**self).as_partial_reflect_mut()
-    }
-
-    fn try_into_reflect(self: Box<Self>) -> Result<Box<dyn Reflect>, Box<dyn PartialReflect>> {
-        (*self).try_into_reflect()
-    }
-
-    fn try_as_reflect(&self) -> Option<&dyn Reflect> {
-        (**self).try_as_reflect()
-    }
-
-    fn try_as_reflect_mut(&mut self) -> Option<&mut dyn Reflect> {
-        (**self).try_as_reflect_mut()
-    }
-
-    fn try_apply(&mut self, value: &dyn PartialReflect) -> Result<(), ApplyError> {
-        (**self).try_apply(value)
-    }
-
-    fn reflect_ref(&self) -> ReflectRef {
-        (**self).reflect_ref()
-    }
-
-    fn reflect_mut(&mut self) -> ReflectMut {
-        (**self).reflect_mut()
-    }
-
-    fn reflect_owned(self: Box<Self>) -> ReflectOwned {
-        (*self).reflect_owned()
-    }
-
-    fn clone_value(&self) -> Box<dyn PartialReflect> {
-        (**self).clone_value()
-    }
-}
-
-impl Reflect for Box<TagFilter> {
-    fn into_any(self: Box<Self>) -> Box<dyn Any> {
-        (*self).into_any()
-    }
-
-    fn as_any(&self) -> &dyn Any {
-        (**self).as_any()
-    }
-
-    fn as_any_mut(&mut self) -> &mut dyn Any {
-        (**self).as_any_mut()
-    }
-
-    fn into_reflect(self: Box<Self>) -> Box<dyn Reflect> {
-        (*self).into_reflect()
-    }
-
-    fn as_reflect(&self) -> &dyn Reflect {
-        (**self).as_reflect()
-    }
-
-    fn as_reflect_mut(&mut self) -> &mut dyn Reflect {
-        (**self).as_reflect_mut()
-    }
-
-    fn set(&mut self, value: Box<dyn Reflect>) -> Result<(), Box<dyn Reflect>> {
-        (**self).set(value)
-    }
-}
-
-impl FromReflect for Box<TagFilter> {
-    fn from_reflect(reflect: &dyn PartialReflect) -> Option<Self> {
-        TagFilter::from_reflect(reflect).map(Box::new)
-    }
-}
-
-impl TypePath for Box<TagFilter> {
-    fn type_path() -> &'static str {
-        TagFilter::type_path()
-    }
-
-    fn short_type_path() -> &'static str {
-        TagFilter::short_type_path()
-    }
-}
-
-impl Typed for Box<TagFilter> {
-    fn type_info() -> &'static TypeInfo {
-        static CELL: NonGenericTypeInfoCell = NonGenericTypeInfoCell::new();
-        CELL.get_or_set(|| TypeInfo::Opaque(OpaqueInfo::new::<Self>()))
-    }
-}
-
-impl GetTypeRegistration for Box<TagFilter> {
-    fn get_type_registration() -> TypeRegistration {
-        TypeRegistration::of::<Self>()
-    }
-}
-
-pub fn none() -> TagFilter {
-    TagFilter::Eq(().into_tags())
-}
-
-pub fn any() -> TagFilter {
-    TagFilter::Any
-}
-
-pub fn all(all: impl IntoTags) -> TagFilter {
-    TagFilter::All(all.into_tags())
-}
-
-pub fn some(some: impl IntoTags) -> TagFilter {
-    TagFilter::Some(some.into_tags())
-}
-
-pub fn not(not: impl Into<TagFilter>) -> TagFilter {
-    !not.into()
-}
-
-pub trait TagFilterOr {
-    fn or(self, other: impl Into<TagFilter>) -> TagFilter;
-}
-
-impl<T: Into<TagFilter>> TagFilterOr for T {
-    fn or(self, other: impl Into<TagFilter>) -> TagFilter {
-        self.into() | other.into()
-    }
-}
-
-pub trait TagFilterAnd {
-    fn and(self, other: impl Into<TagFilter>) -> TagFilter;
-}
-
-impl<T: Into<TagFilter>> TagFilterAnd for T {
-    fn and(self, other: impl Into<TagFilter>) -> TagFilter {
-        self.into() & other.into()
-    }
-}
-
 pub fn matches(tags: impl IntoTags, filter: impl Into<TagFilter>) -> bool {
     filter.into().allows(&tags.into_tags())
 }
@@ -697,69 +443,69 @@ mod tests {
     #[test]
     fn match_empty() {
         assert!(matches((), ()));
-        assert!(matches((), any()));
-        assert!(matches((), all(())));
-        assert!(matches((), none()));
-        assert!(matches((), any() | ()));
-        assert!(matches((), any() | none()));
-        assert!(matches((), any() & ()));
-        assert!(matches((), any() & none()));
+        assert!(matches((), TagFilter::any()));
+        assert!(matches((), TagFilter::all(())));
+        assert!(matches((), TagFilter::none()));
+        assert!(matches((), TagFilter::any() | ()));
+        assert!(matches((), TagFilter::any() | TagFilter::none()));
+        assert!(matches((), TagFilter::any() & ()));
+        assert!(matches((), TagFilter::any() & TagFilter::none()));
 
         assert!(!matches((), !TagFilter::from(())));
-        assert!(!matches((), !any()));
-        assert!(!matches((), !none()));
+        assert!(!matches((), !TagFilter::any()));
+        assert!(!matches((), !TagFilter::none()));
         assert!(!matches((), A));
         assert!(!matches((), [A, B]));
-        assert!(!matches((), some(A)));
-        assert!(!matches((), some([A, B])));
+        assert!(!matches((), TagFilter::some(A)));
+        assert!(!matches((), TagFilter::some([A, B])));
     }
 
     #[test]
     fn match_tag() {
         assert!(matches(A, A));
-        assert!(matches(A, any()));
-        assert!(matches(A, all(A)));
+        assert!(matches(A, TagFilter::any()));
+        assert!(matches(A, TagFilter::all(A)));
         assert!(matches(A, A | B));
         assert!(matches(A, B | A));
-        assert!(matches(A, !none()));
+        assert!(matches(A, !TagFilter::none()));
         assert!(matches(A, !B));
         assert!(matches(A, !C));
-        assert!(matches(A, some(A)));
-        assert!(matches(A, some([A, B])));
-        assert!(matches(B, some([A, B])));
+        assert!(matches(A, TagFilter::some(A)));
+        assert!(matches(A, TagFilter::some([A, B])));
+        assert!(matches(B, TagFilter::some([A, B])));
 
         assert!(!matches(A, B));
-        assert!(!matches(A, none()));
-        assert!(!matches(A, all(B)));
-        assert!(!matches(A, all([A, B])));
+        assert!(!matches(A, TagFilter::none()));
+        assert!(!matches(A, TagFilter::all(B)));
+        assert!(!matches(A, TagFilter::all([A, B])));
         assert!(!matches(A, B | C));
         assert!(!matches(A, !A));
         assert!(!matches(A, [A, B]));
-        assert!(!matches(A, some([B, C])));
+        assert!(!matches(A, TagFilter::some([B, C])));
     }
 
     #[test]
     fn match_tags() {
         assert!(matches([A, B], [A, B]));
         assert!(matches([A, B], [B, A]));
-        assert!(matches([A, B], any()));
-        assert!(matches([A, B], all([A])));
-        assert!(matches([A, B], all([A, B])));
-        assert!(matches([A, B], [A, B].or([B, C])));
-        assert!(matches([A, B], !none()));
+        assert!(matches([A, B], TagFilter::any()));
+        assert!(matches([A, B], TagFilter::all([A])));
+        assert!(matches([A, B], TagFilter::all([A, B])));
+        assert!(matches([A, B], TagFilter::all([A, B]) | [B, C]));
+        assert!(matches([A, B], !TagFilter::none()));
         assert!(matches([A, B], !A));
         assert!(matches([A, B], !C));
-        assert!(matches([A, B], not([B, C])));
-        assert!(matches([A, B], some([A, B])));
+        assert!(matches([A, B], TagFilter::not([B, C])));
+        assert!(matches([A, B], TagFilter::some([A, B])));
 
         assert!(!matches([A, B], [A, C]));
-        assert!(!matches([A, B], none()));
-        assert!(!matches([A, B], all([A, C])));
-        assert!(!matches([A, B], all([A, B, C])));
-        assert!(!matches([A, B], [A, C].or([B, C])));
+        assert!(!matches([A, B], TagFilter::none()));
+        assert!(!matches([A, B], TagFilter::all([A, C])));
+        assert!(!matches([A, B], TagFilter::all([A, B, C])));
+        assert!(!matches([A, B], TagFilter::all([A, C]) | [B, C]));
         assert!(!matches([A, B], A));
         assert!(!matches([A, B], [A, B, C]));
-        assert!(!matches([A, B], some(C)));
+        assert!(!matches([A, B], TagFilter::some(C)));
     }
 
     #[test]
